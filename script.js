90902589a1a125553ec0277ef5204bb47950ecb6
livdetail <126690557+livdetail@users.noreply.github.com>
2024-06-05 09:49:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const caixaPrincipal = documentquerySelector(".caixa-principal")
+const caixaPerguntas = documentquerySelector(".caixa-perguntas")
+const caixaAlternativas = documentquerySelector(".caixa-alternativas")
+const caixaResultavos = documentquerySelector(".caixa-resultados")
+const textoResultavos = documentquerySelector(".texto-resultados")
+
+const perguntas = [
+    {
+        enunciado: "Pergunta1"
+        
+    alternativa: ["Alternativa1", "Alternativa2"]
+
+    },
+    {
+        enunciado: "Pergunta1"
+    alternativa: ["Alternativa1", "Alternativa2"]
+
+    },
+]
